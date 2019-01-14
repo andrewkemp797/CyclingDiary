dcd4cfb16f3753acf750700662e1a791a2b556b8
Andrew <andrew.kemp02@gmail.com>
2019-01-14 20:38:13 +0200

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app.component/app.component';
 import { FeedComponent } from './components/feed.component/feed.component';
 import { EventComponent } from './components/event.component/event.component';
 import { EventDirective } from './directives/event.directive';
 import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
+import { LoginComponent } from './components/login.component/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
     EventComponent,
-    EventDirective, 
+    EventDirective,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule, 
     FormsModule,
     ModalModule.forRoot(),
-    DlDateTimePickerDateModule
+    DlDateTimePickerDateModule,
   ],
   providers: [], 
   entryComponents: [EventComponent],
